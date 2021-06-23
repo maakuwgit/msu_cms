f68be0f6c53500e61c85533f52e2a90a8e107945
WIN-SDBQ3R2H572\PlayerAdmin <mwilliamson@bluewatertech.com>
2021-06-23 00:45:11 -0400

    Updated popper and publish files
    The publish files are likely junk

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import cms from './cms.json'
 
-export const devURL = `//0.0.0.0`
+export const devURL = `//127.0.0.1:8000`
 export const prodURL = window.location.host
 export const baseURL = cms.settings.is_dev ? devURL : prodURL
 
