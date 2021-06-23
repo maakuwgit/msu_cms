dea05907025f27b6fad8c42c1f4724a634ae3651
MaakuW <maakuwemail@gmail.com>
2021-06-23 11:51:17 -0400

    Global fix for the api.js
    Should allow it to work globally. This change keeps getting lost with the forking

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,10 +1,5 @@
 import axios from 'axios'
-import cms from './cms.json'
-
-export const devURL = `//127.0.0.1:8000`
-export const prodURL = window.location.host
-export const baseURL = cms.settings.is_dev ? devURL : prodURL
 
 export default axios.create({
-  baseURL: baseURL
+  baseURL: '//'+window.location.host
 })
