37073b0da7fdaa556cf260145dc1ad1f1f6fb589
MaakuW <maakuwemail@gmail.com>
2021-07-20 17:42:52 -0400

    Only use sourcemaps for dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,10 +10,12 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+require('dotenv').config();
+const use_maps = process.env.APP_ENV === 'local' ? true : false;
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/global.scss', 'public/css/global.css')
    .sass('resources/sass/cms.scss', 'public/css/cms.css')
    .sass('resources/sass/screen.scss', 'public/css/screen.css')
-   //.sourceMaps(true, 'source-map')
+   .sourceMaps(use_maps, 'source-map')
    .version();
